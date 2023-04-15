57304b6a26a01ebfb899961469252c3a59b48c30
AryaDewata <madearyadewata@gmail.com>
2023-04-15 17:07:42 +0800

    third commit

--- script.js
+++ script.js
@@ -21,15 +21,21 @@ const handleScrollAnimation = () => {
 window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
+setTimeout(() => {
+  document.querySelector(".astronaut").style.animation = "4s alternate-reverse infinite astronaut";
+}, 3000);
 
 // Pesan
 const result = document.querySelector("#result");
 function kirimPesan() {
-  let nama = document.querySelector("#nama").value;
-  let pesan = document.querySelector("#pesan").value;
+  let nama = document.querySelector("#nama").value.trim();
+  let pesan = document.querySelector("#pesan").value.trim();
+  if (nama.length < 1 || pesan.length < 1) return false;
   result.innerHTML += `
           <div class="py-2 my-2 px-3 shadow-sm rounded-4 overflow-hidden" style="border-top-left-radius: 0 !important;">
                 <p class="mb-0" style="font-size: 14px;">${pesan}</p>
                 <p class="mb-0 text-secondary" style="font-size: 12px;">${nama}</p>
             </div>`;
+  document.querySelector("#nama").value = "";
+  document.querySelector("#pesan").value = "";
 }
