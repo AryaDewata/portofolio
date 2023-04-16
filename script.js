e7fb91fbea1a8d7bbedd57942c37718871dda765
AryaDewata <madearyadewata@gmail.com>
2023-04-16 18:41:02 +0800

    fifth commit

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ setTimeout(() => {
 }, 3000);
 
 // Pesan
+const bulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
 const result = document.querySelector("#result");
 const nama = document.querySelector("#nama");
 const pesan = document.querySelector("#pesan");
@@ -33,10 +34,13 @@ function kirimPesan() {
   if (nama.value.trim().length < 1) return nama.focus();
   if (pesan.value.trim().length < 1) return pesan.focus();
   result.innerHTML += `
-          <div class="py-2 my-2 px-3 shadow-sm rounded-4 overflow-hidden" style="border-top-left-radius: 0 !important;">
-                <p class="mb-0" style="font-size: 14px;">${pesan.value.trim()}</p>
-                <p class="mb-0 text-secondary" style="font-size: 12px;">${nama.value.trim()}</p>
-            </div>`;
+    <div class="py-2 my-2 px-3 shadow-sm rounded-4 overflow-hidden" style="border-top-left-radius: 0 !important;">
+      <p class="mb-0" style="font-size: 14px; color: rgb(100, 100, 100);">${pesan.value.trim()}</p>
+      <div class="d-flex justify-content-between">
+          <p class="mb-0 text-secondary" style="font-size: 12px;">Dikirim oleh: ${nama.value.trim()}</p>
+          <p class="mb-0 text-secondary" style="font-size: 12px;">~ ${new Date().getDate()} ${bulan[new Date().getMonth()]} ${new Date().getFullYear()}</p>
+      </div>
+    </div>`;
   nama.value = "";
   pesan.value = "";
 }
